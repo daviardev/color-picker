f2c0b3d668fdfb6f06941f29ea541befded36e66
devsilva <jerson.silva99@hotmail.com>
2023-01-03 02:50:51 -0500

    feat: Send and get colors from localstorage and get list of the colors

--- main.js
+++ main.js
@@ -3,7 +3,22 @@ import { $ } from './utils/dom'
 import './style.css'
 
 const colorPicker = $('#color__picker')
-const pickedColors = []
+const colorList = $('.all__colors')
+
+// Seleccionamos lo que esté dentro del local storage, sí no hay nada, será un array vacío
+const pickedColors = JSON.parse(localStorage.getItem('picked__colors') || '[]')
+
+// Mostramos los colores según sean seleccionados
+const showColors = () => {
+    colorList.innerHTML = pickedColors.map(color => `
+        <li class="color">
+            <span class="rect"></span>
+            <span class="value">${color}</span>
+        </li>
+        `)
+        // Generando la lista con los colores seleccionados para añadirlos al cuerpo de los colores
+        .join('')
+}
 
 const activateEyeDropper = async () => {
     try {
@@ -19,8 +34,12 @@ const activateEyeDropper = async () => {
         // Agrega el valor hexadecimal a los colores que fueron seleccionados dentro de un array
         pickedColors.push(sRGBHex)
 
-        console.log(pickedColors)
-    } catch(err) {
+        // Se guarda el valor hexadecimal en el localstorage para que el usuario tenga acceso a ellos
+        // de una forma más sencilla.
+        localStorage.setItem('picked__colors', JSON.stringify(pickedColors))
+
+        showColors()
+    } catch (err) {
         console.error(err)
     }
 }
