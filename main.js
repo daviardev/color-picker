4e1339493ca2a51da836a7779a01f05a303c3b17
devsilva <jerson.silva99@hotmail.com>
2023-01-03 02:29:30 -0500

    feat: Create funcionality for open util and select color

--- main.js
+++ main.js
@@ -2,4 +2,29 @@ import { $ } from './utils/dom'
 
 import './style.css'
 
+const colorPicker = $('#color__picker')
+const pickedColors = []
+
+const activateEyeDropper = async () => {
+    try {
+        // Creamos la propiedad EyeDropper para que se nos abra la herramienta de selección
+        const eyeDropper = new EyeDropper()
+
+        // Guarda El hexadecimal del color seleccionado
+        const { sRGBHex } = await eyeDropper.open()
+
+        // Se pasa el valor hexadecimal al portapapeles
+        navigator.clipboard.writeText(sRGBHex)
+
+        // Agrega el valor hexadecimal a los colores que fueron seleccionados dentro de un array
+        pickedColors.push(sRGBHex)
+
+        console.log(pickedColors)
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+colorPicker.addEventListener("click", activateEyeDropper)
+
 $('#app')
