a07af59f2d90550f5f2a1e39f5143c406ae87435
devsilva <jerson.silva99@hotmail.com>
2023-01-03 03:24:44 -0500

    feat: Copy color

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-import { $ } from './utils/dom'
+import { $, $$ } from './utils/dom'
 
 import './style.css'
 
 const colorPicker = $('#color__picker')
 const colorList = $('.all__colors')
+
+const copyColor = el => {
+    navigator.clipboard.writeText(el.dataset.color)
+    el.innerText = 'copiado'
+    setTimeout(() => el.innerText = el.dataset.color, 1000)
+}
 
 // Seleccionamos lo que esté dentro del local storage, sí no hay nada, será un array vacío
 const pickedColors = JSON.parse(localStorage.getItem('picked__colors') || '[]')
@@ -14,7 +20,7 @@ const showColors = () => {
         <li class="color">
             <span class="rect"
                 style="background: ${color}; border: 1px solid ${color === '#ffffff' ? '#ccc' : color}"></span>
-            <span class="value">${color}</span>
+            <span class="value" data-color="${color}">${color}</span>
         </li>
         `)
         // Generando la lista con los colores seleccionados para añadirlos al cuerpo de los colores
@@ -22,6 +28,11 @@ const showColors = () => {
 }
 
 showColors()
+
+// Añadir un evento al dar clic para copiar el elemento deseado
+$$('.color').forEach(li => {
+    li.addEventListener('click', e => copyColor(e.currentTarget.lastElementChild))
+})
 
 const activateEyeDropper = async () => {
     try {
